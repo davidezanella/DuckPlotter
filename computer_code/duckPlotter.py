59aac2390743e85af8693f3430eb3f4d71b6d610
Davide <davide.zanella10@gmail.com>
2019-04-14 21:17:12 +0200

    Add readData python function

--- computer_code/duckPlotter.py
+++ computer_code/duckPlotter.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
 import serial
 import json
+import time
 
-serialDev = '/dev/ttyUSB0'
+serialDev = '/dev/tty20'
 baudRate = 9600
 
+ser = serial.Serial(serialDev, baudRate, timeout=1)
+
+
 def sendData(obj):
-    data = json.dumps(obj)
-    ser = serial.Serial(serialDev, baudRate, timeout=1)
+    data = json.dumps(obj) + "\n"
     for d in data:
-        print (d)
-        ser.write(str(d))                            
-    ser.close()
+        ser.write(str(d))
+
+    print("Sent:" + data)
+
+def readData():
+    return ser.readline()
 
 def main():
     data = {
         "type": "move"
     }
 
-    sendData(data)
+    for i in range(5):
+        sendData(data)
+        time.sleep(0.5)
+        print("Read: " + readData())
+        
 
 if __name__ == "__main__":
     main()
+                            
+    ser.close()
