84be51e4755492c137ae10385d88ca9dfcf098c5
Davide <davide.zanella10@gmail.com>
2019-08-08 17:11:58 +0200

    Implemented linear and arc movement

--- arduino_code/main.cpp/main.cpp.ino
+++ arduino_code/main.cpp/main.cpp.ino
@@ -1,29 +1,25 @@
-#include "ArduinoJson-v6.10.0.h"
+#include <ArduinoJson.h>
 #include "operations.h"
 
 #define BAUD 9600
-#define STEPPER_SPEED 10 // rpm
-#define MODE MICROSTEP // Micrsostep steps
 
-// Number of steps per output rotation
-// Change this as per your motor's specification
-const int stepsPerRevolution = 200; // For NEMA 17 this value is 200
+#define SERVO_PIN 9
 
-// connect motor to port #2 (M3 and M4)
-AF_Stepper motorX(stepsPerRevolution, 2);
-// connect motor to port #1 (M1 and M2)
-AF_Stepper motorY(stepsPerRevolution, 1);
+
+Servo penMotor;
 
 float lastX, lastY, lastZ;
 
 void setup() {
   Serial.begin(BAUD);
-  motorX.setSpeed(STEPPER_SPEED);
-  motorY.setSpeed(STEPPER_SPEED);
+
+  penMotor.attach(SERVO_PIN);
+  penMotor.write(50);
+
   lastX = lastY = lastZ = 0;
 }
 
-void loop() {
+void start() {
   String input;
   StaticJsonDocument<200> doc;
   if (Serial.available() > 0) {
@@ -37,26 +33,29 @@ void loop() {
       return;
     }
 
-    char* type = doc["type"];
-    Serial.println(type);
+    String type = doc["type"];
 
-    if (type == "movement") {
-      char* moveType = doc["move_type"];
+
+    if (type == "reset") {
+      reset();
+    }
+    else if (type == "movement") {
+      String moveType = doc["move_type"];
       float toX = doc["toX"];
       float toY = doc["toY"];
       bool clockwise = doc["clockwise"];
       if (moveType == "linear") {
-        moveLinear(motorX, motorY, lastX, lastY, toX, toY);
+        moveLinear(lastX, lastY, toX, toY);
       }
       else if (moveType == "arc") {
         if (doc.containsKey("offsetX")) {
           float offsetX = doc["offsetX"];
           float offsetY = doc["offsetY"];
-          moveArc(motorX, motorY, lastX, lastY, toX, toY, offsetX, offsetY, clockwise);
+          moveArc(lastX, lastY, toX, toY, offsetX, offsetY, clockwise);
         }
         else {
           float radius = doc["radius"];
-          moveArc(motorX, motorY, lastX, lastY, toX, toY, radius, clockwise);
+          moveArc(lastX, lastY, toX, toY, radius, clockwise);
         }
       }
       lastX = toX;
@@ -64,7 +63,14 @@ void loop() {
     }
     else if (type == "pen") {
       bool down = doc["down"];
-      movePen(down);
+      movePen(down, penMotor);
     }
+
+    Serial.println("Done");
   }
+}
+
+void loop()
+{
+  start();
 }
--- arduino_code/main.cpp/operations.h
+++ arduino_code/main.cpp/operations.h
@@ -1,80 +1,198 @@
-#include "AFMotor.h"
+#include <Servo.h>
 #include <math.h>
 
-void moveLinear(AF_Stepper motorX, AF_Stepper motorY, float fromX, float fromY, float toX, float toY)
+#include "ShieldDriver.h"
+
+#define X 0
+#define Y 1
+
+#define penUP 50
+#define penDOWN 40
+
+#define maxStepsX 12436 //maximum number of steps on the X axis
+#define maxStepsY 21766 //maximum number of steps on the Y axis
+
+#define numMinSteps 5 //minimum number of steps done
+
+const float scaleXfactor = maxStepsX / 210.0;
+const float scaleYfactor = maxStepsY / 297.0;
+
+const float incrX = numMinSteps / scaleXfactor;
+const float incrY = numMinSteps / scaleYfactor;
+
+float discards[2] = {0, 0};
+
+ShieldDriver driver = ShieldDriver();
+
+/**
+   Check if a number is near enough to a target one
+*/
+bool near(float point, float target)
 {
-    //find the equation of the line (y = mx + q)
-    float m = (toY - fromY) / (toX / fromX);
-    float q = fromY - (m * fromX);
+  const float sens = 0.3;
+  return (target - sens <= point && point <= target + sens);
+}
 
-    float x = fromX;
-    float y = fromY;
+/**
+   Reset the position of all the stepper motors
+*/
+void reset()
+{
+  driver.reset();
+}
 
-    while (x != toX)
+/**
+   Convert a millimenter value to a stepper one in base of the axix
+*/
+float millimetersToSteps(float millimeters, int axis)
+{
+  float scale = scaleXfactor; //X scale factor
+  if (axis == Y)
+    scale = scaleYfactor; //Y scale factor
+
+  return millimeters * scale;
+}
+
+/**
+   Move the stepper motor of the axis by some millimeter value
+*/
+void moveMotor(float mill, int axis)
+{
+  float stepsFloat = millimetersToSteps(mill, axis);
+  int steps = floor(stepsFloat);
+  float discard = stepsFloat - ((float) steps);
+
+  discards[axis] += discard;
+  if (discards[axis] >= 1) 
+  {
+    steps += 1;
+    discards[axis] -= 1;
+  }
+
+  if (axis == X)
+  {
+    driver.motorX.makeSteps(steps);
+  }
+  else if (axis == Y)
+  {
+    driver.motorY.makeSteps(steps);
+  }
+}
+
+/**
+   Make a linear movement
+*/
+void moveLinear(float fromX, float fromY, float toX, float toY)
+{
+  //find the equation of the line (y = mx + q)
+  float m = (toY - fromY) / (toX - fromX);
+  float q = fromY - (m * fromX);
+
+  float x = fromX;
+  float y = fromY;
+
+  while (!near(x, toX) || !near(y, toY))
+  {
+    float nextX = x;
+    float nextY = y;
+
+    if (x != toX) // increment x
     {
-        x++;
-        float nextY = m * x + q;
-        //move motors
+      int sign = (x < toX) ? 1 : -1;
+      nextX += incrX * sign;
+      nextY = m * nextX + q;
     }
-}
-
-void moveArc(AF_Stepper motorX, AF_Stepper motorY, float fromX, float fromY, float toX, float toY, float radius, float centerX, float centerY, bool clockwise)
-{
-    // (x-a)^2 + (y-b)^2 = r^2 where a is centerX, b is centerY and r is the radius
-    // y^2 + (-2b)y + ((x-a)^2 + b^2 - r^2) = 0
-
-    float a = 1;
-    float b = -2 * centerY;
-    float x = fromX;
-    float y = fromY;
-    int incr = (clockwise ? 1 : -1);
-
-    while (x != toX)
+    else // increment y
     {
-        x += incr;
-        float c = pow(x - centerX, 2) + pow(centerY, 2) - pow(radius, 2);
-        float discriminant = b * b - 4 * a * c;
-        float y1 = (-b + sqrt(discriminant)) / (2 * a);
-        float y2 = (-b - sqrt(discriminant)) / (2 * a);
-        float nextY;
-        //choose the right y
-        if (pow(y1 - y, 2) < pow(y2 - y, 2))
-        {
-            nextY = y1;
-        }
-        else
-        {
-            nextY = y2;
-        }
-
-        //move motors
+      int sign = (y < toY) ? 1 : -1;
+      nextY += incrY * sign;
     }
+
+    //move motors
+    moveMotor(nextX - x, X);
+    moveMotor(nextY - y, Y);
+
+    x = nextX;
+    y = nextY;
+  }
 }
 
-void movePen(bool down)
+/**
+   Make an arc movement
+*/
+void moveArc(float fromX, float fromY, float toX, float toY, float radius, float centerX, float centerY, bool clockwise)
 {
+  // x = radius * cos(t) + centerX AND y = radius * sin(t) + centerY
+  // t = atan((y - centerY) / (x - centerX))
+
+  float x = fromX;
+  float y = fromY;
+  float incr = 0.01;
+  incr = (clockwise ? -incr : incr);
+
+  float dx = (x - centerX);
+  float dy = (y - centerY);
+
+  float t = atan(dy / dx);
+
+  //chose the right quadrant
+  if (dx < 0)
+  {
+    t += PI;
+  }
+
+  while (!near(x, toX) || !near(y, toY))
+  {
+    float nextX = radius * cos(t) + centerX;
+    float nextY = radius * sin(t) + centerY;
+
+    //move motors
+    moveMotor(nextX - x, X);
+    moveMotor(nextY - y, Y);
+
+    t += incr;
+    x = nextX;
+    y = nextY;
+  }
 }
 
-void moveArc(AF_Stepper motorX, AF_Stepper motorY, float fromX, float fromY, float toX, float toY, float offsetX, float offsetY, bool clockwise)
+/**
+   Put up or down the pen
+*/
+void movePen(bool down, Servo penMotor)
 {
-    //find the radius
-    float radius = sqrt(pow(offsetX, 2) + pow(offsetY, 2));
-    float centerX = toX + offsetX;
-    float centerY = toY + offsetY;
-
-    moveArc(motorX, motorY, fromX, fromY, toX, toY, radius, centerX, centerY, clockwise);
+  if (down)
+    penMotor.write(penDOWN);
+  else
+    penMotor.write(penUP);
 }
 
-void moveArc(AF_Stepper motorX, AF_Stepper motorY, float fromX, float fromY, float toX, float toY, float radius, bool clockwise)
+/**
+   Make an arc movement
+*/
+void moveArc(float fromX, float fromY, float toX, float toY, float offsetX, float offsetY, bool clockwise)
 {
-    //find the centerX and centerY
-    float radsq = radius * radius;
-    float q = sqrt(pow(fromX - toX, 2) + pow(fromY - toY, 2));
-    float x3 = (fromX + toX) / 2;
-    float centerX = x3 + sqrt(radsq - pow(q / 2, 2) * ((fromY - toY) / q));
+  //find the radius
+  float radius = sqrt(pow(offsetX, 2) + pow(offsetY, 2));
+  float centerX = fromX + offsetX;
+  float centerY = fromY + offsetY;
 
-    float y3 = (fromY + toY) / 2;
-    float centerY = y3 + sqrt(radsq - pow(q / 2, 2) * ((fromX - toX) / q));
+  moveArc(fromX, fromY, toX, toY, radius, centerX, centerY, clockwise);
+}
 
-    moveArc(motorX, motorY, fromX, fromY, toX, toY, radius, centerX, centerY, clockwise);
+/**
+   Make an arc movement
+*/
+void moveArc(float fromX, float fromY, float toX, float toY, float radius, bool clockwise)
+{
+  //find the centerX and centerY
+  float radsq = radius * radius;
+  float q = sqrt(pow(fromX - toX, 2) + pow(fromY - toY, 2));
+  float x3 = (fromX + toX) / 2;
+  float centerX = x3 + sqrt(radsq - pow(q / 2, 2) * ((fromY - toY) / q));
+
+  float y3 = (fromY + toY) / 2;
+  float centerY = y3 + sqrt(radsq - pow(q / 2, 2) * ((fromX - toX) / q));
+
+  moveArc(fromX, fromY, toX, toY, radius, centerX, centerY, clockwise);
 }
